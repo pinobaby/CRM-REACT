4aff49153c1bf78d4235ce9916b9b3dccf30798d
pinobaby <angel.ismael684@gmail.com>
2023-11-22 09:22:57 -0700

    validaciones al formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,6 @@
-import { useNavigate, Form } from "react-router-dom";
+import { useNavigate, Form, useActionData } from "react-router-dom";
 import Formulario from "../components/Formulario";
+import Error from "../components/Error";
 
 export  async function action({request}) {
   const formData = await request.formData()
@@ -7,13 +8,29 @@ export  async function action({request}) {
   const datos = Object.fromEntries(formData)
   
   console.log(datos);
-  
+
+  //validacion de los datos del fomulario
+  const errores = [];
+  if (Object.values(datos).includes("")) {
+    errores.push("Todos los campos son obligatorios" );
+  }
+
+
+  //retornar los errores
+
+  if(Object.keys(errores).length) {
+    return errores
+  }
+
+  console.log(errores);
   return action
 }
 
 function NuevoCliente() {
   const navigate = useNavigate();
+  const errores = useActionData();
 
+  console.log(errores);
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
@@ -31,6 +48,10 @@ function NuevoCliente() {
       </div>
 
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+
+        {errores?.length &&  errores.map( (error, i) =><Error key={i}>{error}</Error>)}
+
+
         <Form method="post">
           <Formulario />
 

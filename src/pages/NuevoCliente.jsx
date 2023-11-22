cd975822cb0f0666478f7589ca99a9e1cee94e6c
pinobaby <angel.ismael684@gmail.com>
2023-11-22 11:40:37 -0700

    validacion de correo en formulario

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -6,6 +6,8 @@ export  async function action({request}) {
   const formData = await request.formData()
   
   const datos = Object.fromEntries(formData)
+  const email = formData.get("email")
+
   
   console.log(datos);
 
@@ -14,7 +16,11 @@ export  async function action({request}) {
   if (Object.values(datos).includes("")) {
     errores.push("Todos los campos son obligatorios" );
   }
+  let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
 
+if(!regex.test(email)) {
+  errores.push("El email no es valido")
+}
 
   //retornar los errores
 
@@ -52,7 +58,8 @@ function NuevoCliente() {
         {errores?.length &&  errores.map( (error, i) =><Error key={i}>{error}</Error>)}
 
 
-        <Form method="post">
+        <Form method="post"
+        noValidate>
           <Formulario />
 
           <input
